d0668792e1c8b5ec5e50921dfbb338c7ef202196
HyoSeong <mongsiry013@naver.com>
2020-04-18 10:57:55 +0900

    Create hyoseong.py

---
+++ source/leetcode/1403/hyoseong.py
@@ -0,0 +1,22 @@
+from collections import deque as dq
+from collections import defaultdict as dd
+from collections import Counter as ct
+from functools import lru_cache as lc
+from heapq import heappush as hpush, heappop as hpop, heapify as hfy
+from bisect import bisect_left, bisect_right
+import itertools as it
+import math
+import sys
+sys.setrecursionlimit(10 ** 9)
+####################################################################
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        nums.sort(key = lambda x : -x)
+        s = sum(nums)
+        ret = []
+        for i in range(len(nums)):
+            if s < sum(ret) :
+                break
+            s -= nums[i]
+            ret.append(nums[i])
+        return ret
