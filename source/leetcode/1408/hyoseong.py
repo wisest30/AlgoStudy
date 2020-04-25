dede8f7961e2c15b07ea1d394acabcea829396fe
HyoSeong <mongsiry013@naver.com>
2020-04-25 11:03:58 +0900

    Create hyoseong.py

---
+++ source/leetcode/1408/hyoseong.py
@@ -0,0 +1,23 @@
+from collections import deque as dq
+from collections import defaultdict as dd
+from collections import Counter as ct
+from functools import lru_cache as lc
+from heapq import heappush as hpush, heappop as hpop, heapify as hfy
+from bisect import bisect_left, bisect_right
+import itertools as it
+import math
+import sys
+sys.setrecursionlimit(10 ** 9)
+####################################################################
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ret = set()
+        n = len(words)
+        for i in range(n):
+            for j in range(n):
+                if i == j :
+                    continue
+                if words[i] in words[j] :
+                    ret.add(words[i])
+        ret = list(ret)
+        return ret
