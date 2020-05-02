cbfa8adfc499210211707783a8ce1a1dce5ca8ac
HyoSeong <mongsiry013@naver.com>
2020-05-02 11:12:20 +0900

    Update hyoseong.py

--- source/leetcode/1417/hyoseong.py
+++ source/leetcode/1417/hyoseong.py
@@ -1,34 +1,25 @@
-from collections import deque as dq
-from collections import defaultdict as dd
-from collections import Counter as ct
-from functools import lru_cache as lc
-from heapq import heappush as hpush, heappop as hpop, heapify as hfy
-from bisect import bisect_left, bisect_right
-import itertools as it
-import math
-import sys
-sys.setrecursionlimit(10 ** 9)
-####################################################################
 class Solution:
     def reformat(self, s: str) -> str:
-        A = []
-        B = []
-        for c in s :
-            if c in "0123456789" :
-                A.append(c)
-            else :
-                B.append(c)
+        digit_str = "0123456789"
+        digits = [c for c in s if c in digit_str]
+        letters = [c for c in s if c not in digit_str]
+
+        digits_cnt = len(digits)
+        letters_cnt = len(letters)
         
-        if len(A) < len(B) :
-            A, B = B, A
-        
-        if len(A) - len(B) > 1 :
+        if abs(digits_cnt - letters_cnt) > 1 :
             return ""
         
         ret = []
-        for i in range(len(A)):
-            ret.append(A[i])
-            if i < len(B) :
-                ret.append(B[i])
+        if digits_cnt > letters_cnt :
+            ret.append(digits.pop())
+        elif letters_cnt > digits_cnt :
+            ret.append(letters.pop())
+        
+        while digits or letters :
+            if ret and ret[-1] in digit_str :
+                ret.append(letters.pop())
+            else :
+                ret.append(digits.pop())
         
         return "".join(ret)
