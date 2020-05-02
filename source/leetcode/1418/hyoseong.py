67ab675a58e6ecb93a07b626ec34fc698a1499c6
HyoSeong <mongsiry013@naver.com>
2020-05-02 11:30:16 +0900

    Update hyoseong.py

--- source/leetcode/1418/hyoseong.py
+++ source/leetcode/1418/hyoseong.py
@@ -1,40 +1,27 @@
-from collections import deque as dq
-from collections import defaultdict as dd
-from collections import Counter as ct
-from functools import lru_cache as lc
-from heapq import heappush as hpush, heappop as hpop, heapify as hfy
-from bisect import bisect_left, bisect_right
-import itertools as it
-import math
-import sys
-sys.setrecursionlimit(10 ** 9)
-####################################################################
+from collections import Counter
 class Solution:
     def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
-        D = dict()
-        food = set()
-        table = set()
-        for o in orders :
-            k = (o[1], o[2])
-            if k in D :
-                D[k] += 1
-            else :
-                D[k] = 1
-            food.add(o[2])
-            table.add(int(o[1]))
+        table_item_counter = Counter()
+        food_set = set()
+        table_set = set()
+        for order in orders :
+            custormerName, tableNumber, foodItem = order
+            tableNumber = int(tableNumber)
+            key = (tableNumber, foodItem)
+            table_item_counter[key] += 1
+            food_set.add(foodItem)
+            table_set.add(tableNumber)
         
-        food = sorted(food)
-        table = sorted(table)
-        ret = [["Table"]]
-        ret[0] += food
-        for t in table :
-            r = [str(t)]
-            for f in food :
-                k = (r[0], f)
-                if k in D :
-                    r.append(str(D[k]))
-                else :
-                    r.append(str(0))
-            ret.append(r)
+        food_list = sorted(food_set)
+        table_list = sorted(table_set)
+
+        ret = []
+        ret.append(["Table"] + food_list)
+        for tableNumber in table_list :
+            row = [str(tableNumber)]
+            for foodItem in food_list :
+                key = (tableNumber, foodItem)
+                row.append(str(table_item_counter[key]))
+            ret.append(row)
         return ret
 
