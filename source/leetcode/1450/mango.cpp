46a1cc6e06f778c048f1c52d647a66884deff777
vanche <vanche9@gmail.com>
2020-05-30 11:26:47 +0900

    Create mango.cpp

---
+++ source/leetcode/1450/mango.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int num = 0;
+        for(int i =0 ;i < startTime.size(); i++){
+            if( queryTime >= startTime[i] && queryTime <= endTime[i])
+                num++;
+        }
+        return num;
+    }
+};
