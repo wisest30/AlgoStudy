16bc12fb817faa6e7fa53e6cdfa57445140232dd
HyoSeong <mongsiry013@naver.com>
2020-06-13 10:59:07 +0900

    Create hyoseong.py

---
+++ source/leetcode/1464/hyoseong.py
@@ -0,0 +1,15 @@
+from collections import deque as dq
+from collections import defaultdict as dd
+from collections import Counter as ct
+from functools import lru_cache as lc
+from heapq import heappush as hpush, heappop as hpop, heapify as hfy
+from bisect import bisect_left, bisect_right
+import itertools as it
+import math
+import sys
+sys.setrecursionlimit(10 ** 9)
+####################################################################
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1] -1) * (nums[-2]-1)
