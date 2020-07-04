048d4d97f9664f6b7f78c008f4c80a509c8b81bc
1ilsang <1ilsang@naver.com>
2020-07-04 11:19:58 +0900

    Create 1ilsang.js

---
+++ source/leetcode/1486/1ilsang.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+const xorOperation = (n, start) => {
+    if(n === 1) return start;
+    
+    let answer = start ^ (start + 2);
+    start += 4;
+    
+    for(let i = 2; i < n; i++) {
+        answer ^= start;
+        start += 2;
+    }
+    
+    return answer;
+};
