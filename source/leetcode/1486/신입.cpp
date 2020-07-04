191b5a7dc17cdcd987ae3f02f384863489cb9937
k1ngee <63134428+k1ngee@users.noreply.github.com>
2020-07-04 11:05:00 +0900

    Create 신입.cpp

---
+++ source/leetcode/1486/신입.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ret = 0;
+        for(int i=0;i<n;i++)
+        {
+            ret^=(start+2*i);
+        }
+        return ret;
+    }
+};
