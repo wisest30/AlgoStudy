18d9d2f5c02622842bef4067141f5dc18699325a
HyoSeong <mongsiry013@naver.com>
2020-07-11 11:08:16 +0900

    Update hyoseong.cpp

--- source/leetcode/1497/hyoseong.cpp
+++ source/leetcode/1497/hyoseong.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        auto A = vector<int>(k+1);
-        long long s = 0;
+        auto cnt = vector<int>(k);
         for(auto a : arr) {
-            auto b = a;
-            if(a < 0)
-                b = a%k + k;            
-            A[b % k]++;
-            s+= b;
+            if(a >= 0) cnt[a % k]++;
+            else cnt[(a % k + k) % k]++;
         }
-        if(s % k) return false;
-        auto n = (int)arr.size();
-        if(n == 2 && A[0] != 2) return false;
-        if(A[0]) return true;
-        for(auto i = 1; i < k; ++i) {
-            if(A[i] && A[k - i] && i != k-i) return true;
-            else if(i == k-i && A[i] >= 2) return true;
-        }
-        return false;
+        if(cnt[0] % 2) return false;
+        for(auto i = 1; i < k; ++i)
+            if(cnt[i] != cnt[k-i]) return false;
+        return true;
     }
 };
