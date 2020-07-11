beb327b8b79685363e46c32f9bd8f59527b7dc26
HyoSeong <mongsiry013@naver.com>
2020-07-11 11:21:41 +0900

    Update hyoseong.cpp

--- source/leetcode/1498/hyoseong.cpp
+++ source/leetcode/1498/hyoseong.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int numSubseq(vector<int>& A, int t) {
+    int numSubseq(vector<int>& nums, int t) {
         constexpr int MOD = 1e9+7;
-        auto B = vector<long long>(A.size()+1);
-        B[0] = 1;
-        for(auto i = 1; i < B.size(); ++i) B[i] = B[i-1] * 2 % MOD;
-        sort(A.begin(), A.end());
+        
+        auto A = vector<long long>{1};
+        for(auto i = 0; i < nums.size(); ++i) A.push_back(A.back() * 2 % MOD);
+        
+        sort(nums.begin(), nums.end());
         long long ret = 0;
-        for(auto i = 0; i < A.size(); ++i) {
-            auto p = upper_bound(A.begin(), A.end(), t - A[i]) - A.begin();
+        for(auto i = 0; i < nums.size(); ++i) {
+            auto p = upper_bound(nums.begin(), nums.end(), t - nums[i]) - nums.begin();
             if(p <= i) break;
-            ret += B[p - i - 1];
-            ret %= MOD;
+            ret = (ret + A[p - i - 1]) % MOD;
         }
         return ret;
     }
