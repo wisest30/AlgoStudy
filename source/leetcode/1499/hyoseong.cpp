7477b113d10b24f54dacbe112d834533f5eef6bf
HyoSeong <mongsiry013@naver.com>
2020-07-11 11:37:05 +0900

    Update hyoseong.cpp

--- source/leetcode/1499/hyoseong.cpp
+++ source/leetcode/1499/hyoseong.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     struct comparePair {
-        bool operator()(pair<int, int>& x, pair<int, int>& y) {
-            return - x.first + x.second < - y.first + y.second;
+        bool operator()(vector<int>& a, vector<int>& b) {
+            return a[0] + a[1] < b[0] + b[1];
         };
     };
     int findMaxValueOfEquation(vector<vector<int>>& P, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, comparePair> q;
-        int n = P.size();
+        priority_queue<vector<int>, vector<vector<int>>, comparePair> q;
         auto ret = -INT_MAX;
-        for(auto i = 0; i < n; ++i) {
-            while(!q.empty() && P[i][0] - q.top().first > k) q.pop();
-            if(!q.empty()) ret = max(ret, P[i][0] - q.top().first + P[i][1] + q.top().second);
-            q.push({P[i][0], P[i][1]});
+        for(auto i = (int)P.size()-1; i >= 0; --i) {
+            auto xi = P[i][0], yi = P[i][1];
+            while(!q.empty() && q.top()[0] - xi > k) q.pop();
+            if(!q.empty()) {
+                auto xj = q.top()[0], yj = q.top()[1];
+                ret = max(ret, yi + yj + abs(xi - xj));
+            }
+            q.push({xi, yi});
         }
         return ret;
     }
