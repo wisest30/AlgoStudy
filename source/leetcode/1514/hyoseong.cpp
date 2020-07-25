0fa91529fefb04ebde20ce5b7a742e7d03a81edf
HyoSeong <mongsiry013@naver.com>
2020-07-25 10:56:00 +0900

    Create hyoseong.cpp

---
+++ source/leetcode/1514/hyoseong.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        auto E = vector<vector<pair<double, int>>>(n);
+        for(auto i = 0; i < edges.size(); ++i) {
+            int u = edges[i][0], v = edges[i][1];
+            auto p = succProb[i];
+            E[u].push_back({p, v});
+            E[v].push_back({p, u});
+        }
+        auto visited = vector<bool>(n);
+        auto q = priority_queue<pair<double, int>>();
+        q.push({1.0, start});
+        while(!q.empty()) {
+            auto cur = q.top().second; auto p = q.top().first; q.pop();
+            cout << cur << " " << p << endl;
+            if(cur == end) return p;
+            if(visited[cur]) {
+                continue;
+            }
+            visited[cur] = true;
+            for(auto& x : E[cur])
+            {
+                auto nxt = x.second;
+                auto nxt_p = p * x.first;
+                q.push({nxt_p, nxt});
+            }
+        }
+        return 0;
+    }
+};
