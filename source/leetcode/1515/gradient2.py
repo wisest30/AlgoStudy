aaa2372fe778d46908563fa35fbf7213124988b8
HyoSeong <mongsiry013@naver.com>
2020-07-25 11:40:31 +0900

    Create gradient2.py

---
+++ source/leetcode/1515/gradient2.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.optimize import minimize
+class Solution:
+    def getMinDistSum(self, positions: List[List[int]]) -> float:
+        x0 = sum([x for x, y in positions]) / len(positions)
+        y0 = sum([y for x, y in positions]) / len(positions)
+        
+        def getCost(pt):
+            avgX, avgY = pt
+            cost = 0
+            for x, y in positions:
+                cost += ((x - avgX) ** 2 + (y - avgY) ** 2) ** 0.5
+            return cost
+        res = minimize(getCost, [x0, y0])
+        return res.fun
