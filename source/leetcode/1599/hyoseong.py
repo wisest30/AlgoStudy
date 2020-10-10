9cc6c081326da4a1b7d9ffa552e239174750afc1
HyoSeong <mongsiry013@naver.com>
2020-10-10 11:11:41 +0900

    Update hyoseong.py

--- source/leetcode/1599/hyoseong.py
+++ source/leetcode/1599/hyoseong.py
@@ -1,21 +1,22 @@
 class Solution:
     def minOperationsMaxProfit(self, customers: List[int], boardingCost: int, runningCost: int) -> int:
         cur = 0
-        A = [0]
-        for i in range(len(customers)) :
-            cur += customers[i]
-            x = min(4, cur)
-            cur -= x
-            A.append(A[-1] + x * boardingCost - runningCost)
-        while cur :
-            x = min(4, cur)
-            cur -= x
-            A.append(A[-1] + x * boardingCost - runningCost)
-        
         idx = -1
         max_val = 0
-        for i in range(len(A)) :
-            if A[i] > max_val :
-                idx = i
-                max_val = A[i]
+        profit = 0
+        i = 0
+        
+        while i < len(customers) or cur > 0 :
+            if i < len(customers) :
+                cur += customers[i]
+            x = min(4, cur)
+            cur -= x
+
+            profit += x * boardingCost - runningCost
+            if profit > max_val :
+                idx = i+1
+                max_val = profit
+
+            i += 1
+
         return idx
