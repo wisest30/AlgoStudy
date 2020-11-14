b2e8007d5a9fd9167d73b47fff1edd3345b4e7ba
HyoSeong <mongsiry013@naver.com>
2020-11-14 11:39:34 +0900

    Update hyoseong.py

--- source/leetcode/1641/hyoseong.py
+++ source/leetcode/1641/hyoseong.py
@@ -1,8 +1,3 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        dp = [1] * 5
-        for i in range(1, n) :
-            for j in range(1, 5) :
-                dp[j] += dp[j-1]
-
-        return sum(dp)
+        return math.comb(n + 4, 4)
