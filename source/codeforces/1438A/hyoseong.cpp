7980e9e7e5451eb3a2901c8fb190284713b11f60
HyoSeong <mongsiry013@naver.com>
2020-11-22 13:38:22 +0900

    Create hyoseong.cpp

---
+++ source/codeforces/1438A/hyoseong.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long;
+
+void solve(int TestCase) {
+    int n;
+    cin >> n;
+
+    for(auto i = 0; i < n; ++i) cout << 1 << " ";
+    cout << endl;    
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    for(auto i = 1; i <= t; ++i) {
+        //cout << "Case #"<< i << ": ";
+        solve(i);
+    }
+}
