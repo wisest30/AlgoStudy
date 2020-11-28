821e621c0a059a05b95ef39d1e1cbe529bfdc989
HyoSeong <mongsiry013@naver.com>
2020-11-28 11:03:38 +0900

    Create hyoseong.py

---
+++ source/leetcode/1657/hyoseong.py
@@ -0,0 +1,6 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1 = Counter(word1)
+        c2 = Counter(word2)
+        
+        return sorted(c1.keys()) == sorted(c2.keys()) and sorted(c1.values()) == sorted(c2.values())
