cbee72370a848b9e2f0960857ab14f5c6f2af598
HyoSeong <mongsiry013@naver.com>
2020-12-05 11:44:46 +0900

    Update hyoseong.py

--- source/leetcode/1665/hyoseong.py
+++ source/leetcode/1665/hyoseong.py
@@ -2,11 +2,9 @@ class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
         tasks.sort(key = lambda x : x[0]-x[1])
         
-        t0, t1 = zip(*tasks)
         ret = s = 0
-        
-        for t in tasks :
-            ret = max(ret, s + t[1])
-            s += t[0]
+        for task in tasks :
+            ret = max(ret, s + task[1])
+            s += task[0]
 
         return ret
