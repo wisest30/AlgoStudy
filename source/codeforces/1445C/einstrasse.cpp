78129c3bf511de9e4d008a6695390934b399f93c
Einstrasse <hg9587@naver.com>
2020-12-10 15:49:08 +0900

    Create einstrasse.cpp

---
+++ source/codeforces/1445C/einstrasse.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl '\n'
+ 
+void solve() {
+	int a, b;
+	cin >> a >> b;
+	a--;
+	cout << a << ' ' << b << endl;
+}
+int main() {
+	int tc;
+	cin >> tc;
+	while (tc--) solve();
+}
