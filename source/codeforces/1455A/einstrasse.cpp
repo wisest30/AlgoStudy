1fe1f72f73885f0fe88b5b7d837a48d98ee58a0e
Einstrasse <hg9587@naver.com>
2020-12-10 15:53:29 +0900

    Create einstrasse.cpp

---
+++ source/codeforces/1455A/einstrasse.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl '\n'
+ 
+void solve() {
+	string s;
+	cin >> s;
+	cout << s.size() << endl;
+}
+int main() {
+	int tc;
+	cin >> tc;
+	while (tc--) solve();
+}
