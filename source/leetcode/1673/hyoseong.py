d6863c670c8550a0399ed31373ca11f5589b6620
HyoSeong <mongsiry013@naver.com>
2020-12-12 11:08:51 +0900

    Update hyoseong.py

--- source/leetcode/1673/hyoseong.py
+++ source/leetcode/1673/hyoseong.py
@@ -1,14 +1,4 @@
-from collections import deque as dq
-from collections import defaultdict as dd
-from collections import Counter as ct
-from functools import lru_cache as lc
 from heapq import heappush as hpush, heappop as hpop, heapify as hfy
-from bisect import bisect_left, bisect_right
-import itertools as it
-import math
-import sys
-sys.setrecursionlimit(10**5)
-####################################################################
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
@@ -24,7 +14,5 @@ class Solution:
             hpush(q, [nums[i], i])
             A.append(hpop(q))
         
-        ret = []
-        for a in A :
-            ret.append(a[0])
+        ret = [a[0] for a in A]
         return ret
