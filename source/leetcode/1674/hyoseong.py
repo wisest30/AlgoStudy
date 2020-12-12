2f8e677d7602c16d634ab2ead4a8a786aff6bbe4
HyoSeong <mongsiry013@naver.com>
2020-12-12 11:26:45 +0900

    Update hyoseong.py

--- source/leetcode/1674/hyoseong.py
+++ source/leetcode/1674/hyoseong.py
@@ -3,20 +3,13 @@ class Solution:
         n = len(nums)
         A = [0] * (2 * 10 ** 5 + 5)
         for i in range(n // 2) :
-            a, b = nums[i], nums[n - 1 - i]
-            if a > b :
-                a, b = b, a
+            a, b = sorted([nums[i], nums[n - 1 - i]])
 
-            left = a + 1
-            right = limit + b
-            A[left] -= 1
+            A[a + 1] -= 1
             A[a + b] -= 1
             A[a + b + 1] += 1
-            A[right + 1] += 1
+            A[limit + b + 1] += 1
         
-        ret = cur = n
-        for i in range(2 * 10 ** 5 + 5) :
-            cur += A[i]
-            ret = min(ret, cur)
-        return ret
+        A[0] += n
+        return min(list(accumulate(A)))
             
