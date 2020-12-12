508f53840a4ed73dbd84f268a7bfc6feee3bbdab
HyoSeong <mongsiry013@naver.com>
2020-12-12 11:54:57 +0900

    Update hyoseong.py

--- source/leetcode/1675/hyoseong.py
+++ source/leetcode/1675/hyoseong.py
@@ -1,29 +1,15 @@
-from collections import deque as dq
-from collections import defaultdict as dd
-from collections import Counter as ct
-from functools import lru_cache as lc
 from heapq import heappush as hpush, heappop as hpop, heapify as hfy
-from bisect import bisect_left, bisect_right
-import itertools as it
-import math
-
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        n = len(nums)
-        q = []
-        min_val = 10 ** 10
-        for i in range(n) :
-            if nums[i] % 2 :
-                nums[i] *= 2
-            hpush(q, -nums[i])
-            min_val = min(min_val, nums[i])
+        q = [-a if a % 2 == 0 else -a * 2 for a in nums]
+        hfy(q)
+        min_val = -max(q)
         
         ret = -q[0] - min_val
-        while True :
+        while q[0] % 2 == 0 :
+            nxt = hpop(q) // 2
+            hpush(q, nxt)
+            min_val = min(min_val, -nxt)
             ret = min(ret, -q[0] - min_val)
-            if (-q[0]) % 2 :
-                break
-            a = hpop(q)
-            min_val = min(min_val, -a // 2)
-            hpush(q, a//2)
+
         return ret
