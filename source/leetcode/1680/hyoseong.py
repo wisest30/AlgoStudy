6110a11a7e43f949ef3e4e4ed4ee426ae19d1c32
HyoSeong <mongsiry013@naver.com>
2020-12-19 11:15:41 +0900

    Update hyoseong.py

--- source/leetcode/1680/hyoseong.py
+++ source/leetcode/1680/hyoseong.py
@@ -1,14 +1,12 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        ret = 0
-        x = 1
         MOD = 10 ** 9 + 7
-        s = len(bin(n)) - 2
-        for i in range(n, 0, -1) :
-            if i < (1 << (s - 1)) :
-                s -= 1
-            ret += i * x
-            x <<= s
-            x %= MOD
-            ret %= MOD
-        return ret
+        
+        cur = 0
+        for i in range(1, n + 1) :
+            l = len(bin(i)) - 2
+            cur <<= l
+            cur += i
+            cur %= MOD
+        
+        return cur
