be4efc10ff5f7012d9334a9fc89817bf93d74f23
HyoSeong <mongsiry013@naver.com>
2021-01-30 11:12:11 +0900

    Update hyoseong.cpp

--- source/leetcode/1726/hyoseong.cpp
+++ source/leetcode/1726/hyoseong.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
+        int ret = 0;
         unordered_map<int, int> M;
         for(auto i = 0; i < nums.size(); ++i)
             for(auto j = i+1; j < nums.size(); ++j)
-                M[nums[i] * nums[j]]++;
+                ret += 8 * M[nums[i] * nums[j]]++;
         
-        int ret = 0;
-        for(auto& p : M)
-            ret += p.second * (p.second - 1);
-        return ret * 4;
+        return ret;
     }
 };
