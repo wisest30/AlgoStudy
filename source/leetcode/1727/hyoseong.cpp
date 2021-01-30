5402a5e27c2f1575b8e68bc1c0c1e44b334d7e7b
HyoSeong <mongsiry013@naver.com>
2021-01-30 11:19:13 +0900

    Update hyoseong.cpp

--- source/leetcode/1727/hyoseong.cpp
+++ source/leetcode/1727/hyoseong.cpp
@@ -4,17 +4,16 @@ public:
         int r = matrix.size();
         int c = matrix[0].size();
         
-        auto A = vector<vector<int>>(r, vector<int>(c));
-        for(auto i = 0; i < r; ++i)
+        for(auto i = 1; i < r; ++i)
             for(auto j = 0; j < c; ++j)
                 if(matrix[i][j])
-                    A[i][j] = (i == 0 ? 0 : A[i-1][j]) + matrix[i][j];
-        
+                    matrix[i][j] += matrix[i-1][j];
+
         int ret = 0;
-        for(auto i = 0; i < r; ++i) {
-            sort(A[i].begin(), A[i].end(), greater<int>());
+        for(auto& A : matrix) {
+            sort(A.begin(), A.end(), greater<int>());
             for(auto j = 0; j < c; ++j)
-                ret = max(ret, (j+1) * A[i][j]);
+                ret = max(ret, A[j] * (j+1));
         }
         
         return ret;
