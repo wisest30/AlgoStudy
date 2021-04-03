2b3157d3552d1b4a8eb267cba155f9e0709c277f
HyoSeong <mongsiry013@naver.com>
2021-04-03 11:52:11 +0900

    Update hyoseong.cpp

--- source/leetcode/1803/hyoseong.cpp
+++ source/leetcode/1803/hyoseong.cpp
@@ -4,33 +4,23 @@ public:
     vector<int> nums;
     int f(int high) {
         auto ret = 0;
-        for(auto x : nums) {
-            auto p = 0;
+        for(auto num : nums) {
+            auto prefix = 0;
             for(auto i = 15; i >= 0; --i) {
-                int z = 16 - i;
-                if(high & (1 << i)) {
-                    if(x & (1 << i)) {
-                        ret += A[z][p+1];
-                    }
-                    else {
-                        ret += A[z][p];
-                        p++;
-                    }
-                }
-                else {
-                    if(x & (1 << i)) {
-                        p++;
-                    }
-                    else {
-                        
-                    }                    
-                }
-                p *= 2;
+                int len = 16 - i;
+                bool p = high & (1 << i);
+                bool q = num & (1 << i);
+                               
+                if(p && q) ret += A[len][prefix + 1];
+                else if(p && !q) ret += A[len][prefix++];
+                else if(!p && q) prefix++;
+                else if(!p && !q); //pass
+                prefix *= 2;
             }
-            p /= 2;
-            ret += A[16][p];
+            ret--;
         }
-        return ret;
+
+        return ret / 2;
     }
     int countPairs(vector<int>& nums, int low, int high) {
         this->nums = nums;
@@ -38,15 +28,15 @@ public:
         for(auto i = 1; i <= 16; ++i)
             A[i].resize(1 << i);
         
-        for(auto x : nums) {
+        for(auto num : nums) {
             for(auto i = 16; i >= 1; --i) {
-                A[i][x]++;
-                x /= 2;
+                A[i][num]++;
+                num >>= 1;
             }
         }
 
         int ret = 0;
-        ret = f(high) - f(low - 1);
-        return ret / 2;
+        ret = f(high + 1) - f(low);
+        return ret;
     }
 };
