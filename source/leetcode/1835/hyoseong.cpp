3fe4f5f24bd9e11a509e9b51b2b3b5ad3f8bad63
HyoSeong <mongsiry013@naver.com>
2021-05-01 11:31:52 +0900

    Update hyoseong.cpp

--- source/leetcode/1835/hyoseong.cpp
+++ source/leetcode/1835/hyoseong.cpp
@@ -4,10 +4,10 @@ public:
         int ret = 0;
         
         for(auto i = 0; i < 32; ++i) {
-            int a = 0, b = 0;
-            for(auto x : arr1) if(x & (1 << i)) a ^= 1;
-            for(auto x : arr2) if(x & (1 << i)) b ^= 1;
-            if(a && b) ret |= 1 << i;
+            long long a = 0, b = 0;
+            for(auto x : arr1) if(x & (1 << i)) a++;
+            for(auto x : arr2) if(x & (1 << i)) b++;
+            if(a * b % 2 == 1) ret |= 1 << i;
         }
         
         return ret;
