a013e64738aba6b86cde5fb0bf08dbefa21b9bfb
HyoSeong <mongsiry013@naver.com>
2021-05-08 11:49:51 +0900

    Update hyoseong2.cpp

--- source/leetcode/1838/hyoseong2.cpp
+++ source/leetcode/1838/hyoseong2.cpp
@@ -1,22 +1,42 @@
+namespace {
+    class SlidingWindow {
+        deque<int> A;
+        int64_t su = 0;
+    public :
+        void push(int x) {
+            A.push_back(x);
+            su += x;
+        }
+
+        void pop() {
+            su -= A.front();
+            A.pop_front();
+        }
+
+        int64_t get_need() {
+            return (int64_t)A.size() * A.back() - su;
+        }
+
+        size_t size() {
+            return A.size();
+        }
+    };
+}
+
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         
-        int64_t ret = 1, su = nums[0], i = 1, j = 0;
-        while(i <= nums.size()) {
-            auto need = nums[i-1] * (i - j) - su;
-            while(need > k) {
-                su -= nums[j++];
-                need = nums[i-1] * (i - j) - su;
-            }
-            
-            ret = max(ret, i - j);
-
-            if(i < nums.size())
-                su += nums[i];
-            i++;
+        SlidingWindow sw;
+        auto ret = 0;
+        for(auto x : nums) {
+            sw.push(x);
+            while(sw.get_need() > k)
+                sw.pop();
+            ret = max(ret, (int)sw.size());
         }
+        
         return ret;
     }
 };
