3fe2675483c91b181cf55c3c845f43322066555c
HyoSeong <mongsiry013@naver.com>
2021-05-15 11:00:02 +0900

    Create hyoseong.cpp

---
+++ source/leetcode/1849/hyoseong.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool f(const string& s, unsigned long long start) {
+        if(s.empty())
+            return true;
+        
+        if(start == 0) {
+            for(auto c : s)
+                if(c != '0')
+                    return false;
+            
+            return true;
+        }
+        
+        int i = 0;
+        unsigned long long cur = 0;
+        for(i = 0; i < s.size(); ++i) {
+            auto nxt = cur * 10 + s[i] - '0';
+            if(nxt > start)
+                break;
+            cur = nxt;
+        }
+        
+        if(cur != start)
+            return false;
+        
+        return f(s.substr(i), start - 1);
+    }
+    
+    bool splitString(string s) {
+        if(f(s, 0))
+            return false;
+        
+        for(auto i = 1; i < s.size(); ++i) {
+            unsigned long long start = stoull(s.substr(0, i));
+            if(f(s, start))
+                return true;
+        }
+        
+        return false;
+    }
+};
