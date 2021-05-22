6f6a51fba6b0756da8382ff00e3fd6f43e63592e
HyoSeong <mongsiry013@naver.com>
2021-05-22 11:17:18 +0900

    Update hyoseong.cpp

--- source/leetcode/1855/hyoseong.cpp
+++ source/leetcode/1855/hyoseong.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        reverse(nums2.begin(), nums2.end());
-        
         auto ret = 0;
         for(auto i = 0; i < nums1.size(); ++i) {
-            int p = lower_bound(nums2.begin(), nums2.end(), nums1[i]) - nums2.begin();
+            int p = lower_bound(nums2.rbegin(), nums2.rend(), nums1[i]) - nums2.rbegin();
             int j = (int)nums2.size() - p - 1;
             if(j < 0 || j < i) continue;
             ret = max(ret, j - i);
