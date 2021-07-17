91eda9f4f07ba25c2ece425bb69de515930719de
HyoSeong <mongsiry013@naver.com>
2021-07-17 10:08:37 +0900

    Create hyoseong_failed2.cpp

---
+++ source/leetcode/1923/hyoseong_failed2.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int longestCommonSubpath(int n, vector<vector<int>>& paths) {
+        constexpr int MOD = 1e9 + 7;
+        int m = paths.size();
+        if(m == 1) return paths[0].size();
+        
+        int min_sz = INT_MAX;
+        for(auto& p : paths)
+            min_sz = min(min_sz, (int)p.size());
+        
+        auto check = [&](auto mm, auto q) {
+            vector<set<int>> S(m);
+            for(auto i = 0; i < m; ++i) {
+                int64_t h = 0;
+                int64_t p = 1;
+                for(auto j = 0; j < mm; ++j) {
+                    h = h * q % MOD + paths[i][j];
+                    h %= MOD;
+                    if(j != mm - 1)
+                        p = p * q % MOD;
+                }
+                S[i].insert(h);
+                for(auto j = mm; j < paths[i].size(); ++j) {
+                    int64_t prev = paths[i][j - mm];
+                    h = (h - prev * p % MOD + MOD) % MOD;
+                    h = h * q % MOD + paths[i][j];
+                    h %= MOD;
+                    S[i].insert(h);
+                }
+            }
+            
+            for(auto h : S[0]) {
+                bool success = true;
+                for(auto i = 1; i < m; ++i)
+                    if(S[i].count(h) == 0) {
+                        success = false;
+                        break;
+                    }
+                
+                if(success) return true;
+            }
+            
+            return false;
+        };
+        
+        int l = 0, r = min_sz + 1;
+        while(l + 1 < r) {
+            auto m = (l + r) / 2;
+            if(check(m, 12345678ll) && check(m, 58372ll)) l = m;
+            else r = m;
+        }
+        
+        return l;
+    }
+};
