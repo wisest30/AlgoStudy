a43e2196b1cfc8a0d6a5a24811781d608ef03731
Einstrasse <hg9587@naver.com>
2021-08-12 16:53:07 +0900

    Create einstrasse.cc

---
+++ source/leetcode/1952/einstrasse.cc
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int div = 0;
+        for (int i=1; i <= n; i++) {
+            if (n % i == 0) div++;
+        }
+        return div == 3;
+    }
+};
