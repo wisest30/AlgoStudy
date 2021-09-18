f0298896403e4386451d2a79896d6ffd8f15d520
HyoSeong <mongsiry013@naver.com>
2021-09-19 01:38:38 +0900

    Update hyo.rs

--- source/leetcode/2007/hyo.rs
+++ source/leetcode/2007/hyo.rs
@@ -1,16 +1,31 @@
+use std::collections::HashMap;
+
 impl Solution {
-    pub fn count_k_difference(nums: Vec<i32>, k: i32) -> i32 {
-        let mut ret = 0;
-        let n = nums.len();
-        
-        for i in 0..n {
-            for j in i+1..n {
-                if (nums[i] - nums[j]).abs() == k {
-                    ret += 1;
+    pub fn find_original_array(changed: Vec<i32>) -> Vec<i32> {
+        let mut m = HashMap::new();
+        for &x in &changed {
+            let p = m.entry(x).or_insert(0);
+            *p += 1;
+        }
+
+        let mut ret = Vec::new();
+        for i in 0..=100000 {
+            while m.contains_key(&i) && *m.get(&i).unwrap() > 0 {
+                let p = m.entry(i).or_insert(0);
+                *p -= 1;
+                
+                let j = i * 2;
+                if !m.contains_key(&j) || *m.get(&j).unwrap() == 0 {
+                    return vec![0; 0];
+                } else {
+                    let q = m.entry(j).or_insert(0);
+                    *q -= 1;
+                    
+                    ret.push(i);
                 }
             }
         }
-        
+                
         ret
     }
 }
