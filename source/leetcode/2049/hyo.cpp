a795223465f83f23b1de4ca6e74f99634a35c146
HyoSeong <mongsiry013@naver.com>
2021-10-31 19:13:16 +0900

    Update hyo.cpp

--- source/leetcode/2049/hyo.cpp
+++ source/leetcode/2049/hyo.cpp
@@ -2,29 +2,15 @@ class Solution {
 public:
     int n;
     vector<vector<int>> g;
-    // return type : (node cnt, max_score, max_score_cnt)
-    array<int64_t, 3> dfs(int cur) {
-        int64_t cnt = 1, score = 1, max_score = 1, max_score_cnt = 0;
+    vector<int64_t> cnts, scores;
+    void dfs(int cur) {
         for(auto nxt : g[cur]) {
-            auto sub_ret = dfs(nxt);
-            cnt += sub_ret[0];
-            score *= sub_ret[0];
-            
-            if(sub_ret[1] == max_score)
-                max_score_cnt += sub_ret[2];
-            else if(sub_ret[1] > max_score)
-                max_score = sub_ret[1], max_score_cnt = sub_ret[2];
+            dfs(nxt);
+            cnts[cur] += cnts[nxt];
+            scores[cur] *= cnts[nxt];
         }
-
-        if(cur)
-            score *= n - cnt;
         
-        if(score == max_score)
-            ++max_score_cnt;
-        else if(score > max_score)
-            max_score = score, max_score_cnt = 1;
-        
-        return {cnt, max_score, max_score_cnt};
+        if(cur) scores[cur] *= n - cnts[cur];        
     }
     
     int countHighestScoreNodes(vector<int>& parents) {
@@ -32,6 +18,13 @@ public:
         g = vector<vector<int>>(n);
         for(auto i = 1; i < n; ++i)
             g[parents[i]].push_back(i);
-        return dfs(0)[2];
+        
+        cnts = scores = vector<int64_t>(n, 1);
+        dfs(0);
+        
+        map<int64_t, int> counter;
+        for(auto s : scores) counter[s]++;
+        
+        return counter.rbegin()->second;
     }
 };
