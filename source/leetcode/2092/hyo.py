fa195a7e4ac2bc5ca3aaf33073c72ee48caa85d8
HyoSeong <mongsiry013@naver.com>
2021-11-28 14:17:38 +0900

    Update hyo.py

--- source/leetcode/2092/hyo.py
+++ source/leetcode/2092/hyo.py
@@ -1,6 +1,6 @@
 class Solution:
-    def findAllPeople(self, n: int, M: List[List[int]], f: int) -> List[int]:
-        M.sort(key = lambda x : x[2])
+    def findAllPeople(self, n: int, meetings: List[List[int]], f: int) -> List[int]:
+        meetings.sort(key = lambda x : x[2])
         know = [False] * n
         know[0] = know[f] = True
         
@@ -12,21 +12,15 @@ class Solution:
             for nxt in g[cur] :
                 dfs(nxt, g, visited)
         
-        left = right = 0
-        while left < len(M) :
-            while right < len(M) and M[left][2] == M[right][2] :
-                right += 1
-            
+        for _, M in groupby(meetings, key = lambda x : x[2]) :
             g = defaultdict(list)
-            for k in range(left, right) :
-                g[M[k][0]].append(M[k][1])
-                g[M[k][1]].append(M[k][0])
+            for e in M :
+                g[e[0]].append(e[1])
+                g[e[1]].append(e[0])
             
             visited = set()
             for i in g :
                 if know[i] :
                     dfs(i, g, visited)
-            
-            left = right
         
         return [i for i in range(n) if know[i]]
