d25e8035d1b16f38c592c42cb4200f3929143ad2
HyoSeong <mongsiry013@naver.com>
2022-01-07 18:27:45 +0900

    Create hyo.rs

---
+++ source/leetcode/2125/hyo.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn number_of_beams(bank: Vec<String>) -> i32 {
+        let mut prev = 0;
+        bank.iter()
+            .filter(|s| s.contains("1"))
+            .map(|s| s.chars().filter(|&c| c == '1')
+            .count())
+            .collect::<Vec<_>>()
+            .iter()
+            .fold(0, |acc, &x| { 
+                let ret = acc + x * prev;
+                prev = x;
+                ret
+            }) as i32
+    }
+}
