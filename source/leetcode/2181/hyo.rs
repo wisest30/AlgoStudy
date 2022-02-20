927c4b3c8063ee7c9ddfa9c0a861fa438bc51578
HyoSeong <mongsiry013@naver.com>
2022-02-20 15:33:35 +0900

    Update hyo.rs

--- source/leetcode/2181/hyo.rs
+++ source/leetcode/2181/hyo.rs
@@ -1,19 +1,3 @@
-// Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-// 
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
 impl Solution {
     pub fn merge_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut v = Vec::new();
