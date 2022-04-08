576da4e6c3ba11f2dc7845837b830dd52c25d962
rrrohyy <noyunyoung@naver.com>
2022-04-09 02:26:58 +0900

    Create yy.py

---
+++ source/leetcode/2225/yy.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        dict_win={}
+        dict_lose={}
+        total_nums={}
+        for match in matches:
+            i,j=match
+            if i not in total_nums:
+                total_nums[i]=0
+            if j not in total_nums:
+                total_nums[j]=1
+            else:total_nums[j]+=1
+        sorted_dict = sorted(total_nums.items(), key = lambda item: item[0])
+            
+        result=[[],[]]
+        for k,y in sorted_dict:
+            if total_nums[k]==0:
+                result[0].append(k)
+            elif total_nums[k]==1:
+                result[1].append(k)
+       
+        return result
