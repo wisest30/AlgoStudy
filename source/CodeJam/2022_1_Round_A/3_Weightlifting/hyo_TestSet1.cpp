ca121da30e248b47e72ea78748d44d58bce3f8f1
HyoSeong <mongsiry013@naver.com>
2022-04-09 22:10:56 +0900

    Update hyo_TestSet1.cpp

--- source/CodeJam/2022_1_Round_A/3_Weightlifting/hyo_TestSet1.cpp
+++ source/CodeJam/2022_1_Round_A/3_Weightlifting/hyo_TestSet1.cpp
@@ -3,45 +3,52 @@
 using namespace std;
 using ll = long long;
 
-int e, w;
-vector<vector<int>> A, dp;
-
-int recur(int l, int r) {
-    if(l + 1 == r)
-        return 2 * accumulate(A[l].begin(), A[l].end(), 0);
-    
-    auto& ret = dp[l][r];
-    if(ret != -1)
-        return ret;
-    
-    vector<int> minv(w, INT_MAX);
-    for(auto i = l; i < r; ++i)
-        for(auto j = 0; j < w; ++j)
-            minv[j] = min(minv[j], A[i][j]);
-    auto su = accumulate(minv.begin(), minv.end(), 0);
-
-    ret = INT_MAX;
-    for(auto i = l + 1; i < r; ++i) {
-        auto sub_ret0 = recur(l, i);
-        auto sub_ret1 = recur(i, r);
-
-        ret = min(ret, sub_ret0 + sub_ret1 - 2 * su);
+int f(const string& from, const string& to) {
+    int n = min(from.size(), to.size());
+    int ret = from.size() + to.size();
+    for(auto i = 0; i < n; ++i) {
+        if(from[i] == to[i])
+            ret -= 2;
+        else
+            break;
     }
 
     return ret;
 }
 
 void solve(int TestCase) {
+    int e, w;
     cin >> e >> w;
 
-    A = vector<vector<int>>(e, vector<int>(w));
-    for(auto& arr : A)
-        for(auto& x : arr)
-            cin >> x;
+    vector<pair<int, string>> P;
+    P.push_back({0, ""});
+    for(auto i = 0; i < e; ++i) {
+        string A;
+        for(auto j = 0; j < w; ++j) {
+            int cnt;
+            cin >> cnt;
+            for(auto k = 0; k < cnt; ++k)
+                A.push_back('0' + j);
+        }
 
-    dp = vector<vector<int>>(e, vector<int>(e + 1, -1));
-    auto ret = recur(0, e);
-    cout << ret << "\n";
+        vector<pair<int, string>> nxt_P;
+        do {
+            int min_v = INT_MAX;
+            for(auto& p : P) {
+                min_v = min(min_v, p.first + f(p.second, A));
+            }
+
+            nxt_P.push_back({min_v, A});
+        } while (next_permutation(A.begin(), A.end()));
+
+        swap(P, nxt_P);
+    }
+
+    int min_v = INT_MAX;
+    for(auto& p : P)
+        min_v = min(min_v, p.first + (int)p.second.size());
+    
+    cout << min_v << "\n";
 }
 
 int main(){
