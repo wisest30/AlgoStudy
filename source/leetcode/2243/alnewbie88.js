5573a23f5ce079970ad1794e025ba082b7693e4a
pairigg48 <hazcoke@gmail.com>
2022-04-24 21:05:40 +0900

    Add files via upload

--- source/leetcode/2243/alnewbie88.js
+++ source/leetcode/2243/alnewbie88.js
@@ -1,42 +1,19 @@
 /**
- * @param {number[]} queries
- * @param {number} intLength
- * @return {number[]}
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
  */
-var kthPalindrome = function(queries, intLength) {
-    let makePalindrome = function(intLength, query) {
-        let maxQuery = 9 * Math.pow(10, Math.floor((intLength - 1) / 2));
-        if (query > maxQuery) {
-            return -1;
+var digitSum = function(s, k) {
+    while (s.length > k) {
+        let s2 = '';
+        for (let i = 0; i < s.length; i += k) {
+            const group = s.substring(i, i + k);
+            let sum = 0;
+            group.split('').forEach(num => sum += parseInt(num));
+            s2 += sum.toString();
         }
-        
-        if (intLength === 1) {
-            return query;
-        }
-        
-        let maxQueryDigits = 0;
-        while (maxQuery > 0) {
-            maxQuery = Math.floor(maxQuery / 10);
-            maxQueryDigits++;
-        }
-        
-        let leftNum = query;
-        if (maxQueryDigits >= 2) {
-            leftNum += parseInt(''.padStart(maxQueryDigits - 1, '9'));
-        }
-        leftNum = leftNum.toString();
-        
-        let palindrome = '';
-        if (intLength % 2 === 1) {
-            const leftNumWithoutLast = leftNum.slice(0, leftNum.length - 1);
-            palindrome = leftNum + leftNumWithoutLast.split('').reverse().join('');
-        } else {
-            palindrome = leftNum + leftNum.split('').reverse().join('');
-        }
-        return parseInt(palindrome);
+        s = s2;
     }
     
-    let ans = [];
-    queries.forEach(query => ans.push(makePalindrome(intLength, query)));
-    return ans;
+    return s;
 };
