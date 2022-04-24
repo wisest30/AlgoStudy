b0767d35d79c728c3db41123c1b4f48fa00bdcd8
pairigg48 <hazcoke@gmail.com>
2022-04-24 21:06:07 +0900

    Add files via upload

--- source/leetcode/2244/alnewbie88.js
+++ source/leetcode/2244/alnewbie88.js
@@ -1,42 +1,29 @@
 /**
- * @param {number[]} queries
- * @param {number} intLength
- * @return {number[]}
+ * @param {number[]} tasks
+ * @return {number}
  */
-var kthPalindrome = function(queries, intLength) {
-    let makePalindrome = function(intLength, query) {
-        let maxQuery = 9 * Math.pow(10, Math.floor((intLength - 1) / 2));
-        if (query > maxQuery) {
-            return -1;
-        }
-        
-        if (intLength === 1) {
-            return query;
-        }
-        
-        let maxQueryDigits = 0;
-        while (maxQuery > 0) {
-            maxQuery = Math.floor(maxQuery / 10);
-            maxQueryDigits++;
-        }
-        
-        let leftNum = query;
-        if (maxQueryDigits >= 2) {
-            leftNum += parseInt(''.padStart(maxQueryDigits - 1, '9'));
-        }
-        leftNum = leftNum.toString();
-        
-        let palindrome = '';
-        if (intLength % 2 === 1) {
-            const leftNumWithoutLast = leftNum.slice(0, leftNum.length - 1);
-            palindrome = leftNum + leftNumWithoutLast.split('').reverse().join('');
+var minimumRounds = function(tasks) {
+    let m = new Map();
+    tasks.forEach(task => {
+        if (m.has(task)) {
+            m.set(task, m.get(task) + 1);
         } else {
-            palindrome = leftNum + leftNum.split('').reverse().join('');
+            m.set(task, 1);
         }
-        return parseInt(palindrome);
-    }
+    });
     
-    let ans = [];
-    queries.forEach(query => ans.push(makePalindrome(intLength, query)));
-    return ans;
+    let minRounds = 0;
+    let impossible = false;
+    m.forEach((value, key) => {
+        if (value === 1) {
+            impossible = true;
+            return;
+        }
+        
+        let div = Math.floor(value / 3);
+        let mod = value % 3;
+        minRounds += mod === 0 ? div : div + 1;
+    });
+    
+    return impossible ? -1 : minRounds;
 };
