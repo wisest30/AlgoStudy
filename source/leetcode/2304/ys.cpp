eb9841ed405e7f9089b62e4b7137ec9ac70acffd
ys-k <43584573+yoonseok-kim@users.noreply.github.com>
2022-06-18 11:50:40 +0900

    Update ys.cpp

--- source/leetcode/2304/ys.cpp
+++ source/leetcode/2304/ys.cpp
@@ -1,52 +1,52 @@
-#include <iostream>
-#include <vector>
+class Solution
+{
 
-using namespace std;
+private:
+	const int LIMIT = 12345;
+	vector<vector<int>> s_grid;
+	vector<vector<int>> s_moveCost;
+	int R, C;
+	int cache[50][50] = {
+		0,
+	};
 
-const int LIMIT = 12345;
-vector<vector<int>> s_grid;
-vector<vector<int>> s_moveCost;
-int R, C;
+	int process(int row, int col)
+	{
+		if (row == R - 1)
+		{
+			return s_grid[row][col];
+		}
 
-int cache[50][50] = {
-	0,
+		if (cache[row][col] > 0)
+		{
+			return cache[row][col];
+		}
+
+		int value = s_grid[row][col];
+		auto &costs = s_moveCost[value];
+
+		int res = LIMIT;
+		for (int c = 0; c < C; ++c)
+		{
+			res = std::min(res, value + costs[c] + process(row + 1, c));
+		}
+
+		return cache[row][col] = res;
+	}
+
+public:
+	int minPathCost(vector<vector<int>> &grid, vector<vector<int>> &moveCost)
+	{
+		R = grid.size();
+		C = grid.front().size();
+		s_grid = std::move(grid);
+		s_moveCost = std::move(moveCost);
+
+		int res = LIMIT;
+		for (int c = 0; c < C; ++c)
+		{
+			res = std::min(res, process(0, c));
+		}
+		return res;
+	}
 };
-
-int process(int row, int col)
-{
-	if (row == R - 1)
-	{
-		return s_grid[row][col];
-	}
-
-	if (cache[row][col] > 0)
-	{
-		return cache[row][col];
-	}
-
-	int value = s_grid[row][col];
-	auto &costs = s_moveCost[value];
-
-	int res = LIMIT;
-	for (int c = 0; c < C; ++c)
-	{
-		res = std::min(res, value + costs[c] + process(row + 1, c));
-	}
-
-	return cache[row][col] = res;
-}
-
-int minPathCost(vector<vector<int>> &grid, vector<vector<int>> &moveCost)
-{
-	R = grid.size();
-	C = grid.front().size();
-	s_grid = std::move(grid);
-	s_moveCost = std::move(moveCost);
-
-	int res = LIMIT;
-	for (int c = 0; c < C; ++c)
-	{
-		res = std::min(res, process(0, c));
-	}
-	return res;
-}
