ec04c715549dc6eeadff7493f5e9c2d3432f1eb5
ys-k <43584573+yoonseok-kim@users.noreply.github.com>
2022-06-18 10:43:11 +0900

    Create ys.cpp

---
+++ source/leetcode/2303/ys.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+double calculateTax(std::vector<std::vector<int>> &brackets, int income)
+{
+	double tax = 0.0;
+	int prev_upper = 0;
+
+	for (size_t i = 0; i < brackets.size(); ++i)
+	{
+		auto &bracket = brackets[i];
+		int upper = bracket.front();
+		double percent = bracket.back() / 100.0f;
+		int diff_upper = upper - prev_upper;
+
+		if (diff_upper > income)
+		{
+			tax += (income * percent);
+		}
+		else
+		{
+			tax += (diff_upper * percent);
+		}
+
+		income -= diff_upper;
+		prev_upper = upper;
+
+		if (income <= 0)
+		{
+			break;
+		}
+	}
+
+	return tax;
+}
