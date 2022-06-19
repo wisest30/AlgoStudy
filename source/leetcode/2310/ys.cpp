d5657dbdfa363d5da37f563236c24790f4171a7a
ys-k <43584573+yoonseok-kim@users.noreply.github.com>
2022-06-19 13:30:53 +0900

    Create ys.cpp

---
+++ source/leetcode/2310/ys.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    static constexpr int UNSET = 12345;
+
+public:
+    int minimumNumbers(int num, int k) {
+        if (num == 0) {
+            return 0;
+        }
+        
+        std::vector<int> cache(num + 1, UNSET);
+        
+        int current = k;
+        std::vector<int> targets;
+        while (current <= num) {
+            cache[current] = 1;
+            targets.push_back(current);
+            current += 10;
+        }
+        
+        for (int i = 0; i <= num; ++i) {
+            for (int j = 0; j < targets.size(); ++j) {
+                if (i < targets[j] || cache[i - targets[j]] == UNSET) {
+                    continue;
+                }
+                
+                cache[i] = std::min(cache[i], cache[i - targets[j]] + 1);
+            }
+        }
+        
+        return cache[num] == 12345 ? -1 : cache[num];
+    }
+};
