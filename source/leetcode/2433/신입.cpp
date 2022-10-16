c4e541c456bee64e7ecea26a27ff813db9792524
k1ngee <63134428+k1ngee@users.noreply.github.com>
2022-10-16 20:46:32 +0900

    Create 신입.cpp

---
+++ source/leetcode/2433/신입.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> ans;
+        ans.push_back(pref[0]);
+        for(int i=1;i<pref.size();i++) ans.push_back(pref[i]^pref[i-1]);
+        return ans;
+    }
+};
